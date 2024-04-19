d08a65e234dce770e898e8e0dcb33f80b1ab62d7
Peyton Meyer <peytonmeyer23@gmail.com>
2024-04-19 10:17:57 -0400

    base

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, text
+
+
+app = Flask(__name__)
+
+
+
+conn_str = "mysql://root:9866@localhost/ecommerce"
+engine = create_engine(conn_str, echo=True)
+conn = engine.connect()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
