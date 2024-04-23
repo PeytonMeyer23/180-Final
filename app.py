e275d56f84e032d63297680a0c5ea63c8a65fef6
Peyton Meyer <peytonmeyer23@gmail.com>
2024-04-23 08:10:49 -0400

    added product stuff

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import flask
 from flask import Flask, render_template, request, redirect, url_for, abort, session
 from sqlalchemy import create_engine, text
 from random import randint
+import sqlite3
+import json
 
 app = Flask(__name__)
-conn_str = 'mysql://root:cset155localhost/ecommerce'
+
+conn_str = "mysql://root:9866@localhost/ecommerce"
 engine = create_engine(conn_str, echo = True)
 conn = engine.connect()
 app.secret_key = 'hello'
@@ -31,9 +34,9 @@ def create_account():
     else:
         return render_template("register.html")
 
-
-
-
-
+@app.route('/products')
+def get_products():
+    products = conn.execute(text("SELECT * FROM product")).fetchall()
+    return render_template("products.html", products=products)
 if __name__ == '__main__':
     app.run(debug=True)
