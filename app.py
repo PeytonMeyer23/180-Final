1b3dc317e1996b0054f8d429a0bc98c397f1e011
Kodee <kodeekong@yahoo.com>
2024-04-29 10:54:16 -0400

    .

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
-conn_str = "mysql://root:CSET@localhost/ecomerce"
+conn_str = "mysql://root:cset155@localhost/ecommerce"
 
 engine = create_engine(conn_str, echo = True)
 conn = engine.connect()
